3889ddd47e7aeb2006a4d91b0084aed6275a6d88
Valentina-Maraio <valentina.maraio89@gmail.com>
2022-12-29 16:32:20 +0100

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { NextUIProvider } from "@nextui-org/react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './views/Home'
+import Noticias from './views/Noticias'
+import InfoNoticia from './views/InfoNoticia'
 
 function App() {
   return (
@@ -8,6 +11,13 @@ function App() {
       <NextUIProvider>
         <div className="App">
           <header className="App-header">
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/noticias" element={<Noticias/>}></Route>
+                <Route path="/info-noticia" element={<InfoNoticia/>}></Route>
+              </Routes>
+            </Router>
           </header>
         </div>
       </NextUIProvider>
---
+++ src/views/InfoNoticia.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InfoNoticia = () => {
+  return (
+    <div>InfoNoticia</div>
+  )
+}
+
+export default InfoNoticia
